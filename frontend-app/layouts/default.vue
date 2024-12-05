65e50fffad375a87de35079a14c4851b56201d46
Fabrício Maxwell <102274424+FabricioMaxwell@users.noreply.github.com>
2024-12-05 13:52:02 -0300

    Sumiit configuração do app

---
+++ frontend-app/layouts/default.vue
@@ -0,0 +1,10 @@
+
+<template>
+    <h1>Layouts</h1>
+    <slot />
+</template>
+
+
+<script setup lang="ts">
+
+</script>
---
+++ frontend-app/pages/index.vue
@@ -0,0 +1,10 @@
+<template>
+    <h1>login</h1>
+</template>
+
+<script setup lang="ts">
+definePageMeta({
+    layout: 'login'
+})
+
+</script>
